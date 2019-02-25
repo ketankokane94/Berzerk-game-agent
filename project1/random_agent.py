9cdd8d273e520f858d99ef84936349914b14de2c
ketankokane94 <ketankokane94@gmail.com>
2019-02-25 15:59:07 -0500

    random agent analysis

---
+++ project1/random_agent.py
@@ -0,0 +1,78 @@
+from random import randint
+import numpy as np
+
+class Agent(object):
+    """The world's simplest agent!"""
+    done_ = 1
+    def __init__(self, action_space):
+        # stores the available actions 
+        '''
+        ACTION_MEANING = {
+    0 : "NOOP",
+    1 : "FIRE",
+    2 : "UP",
+    3 : "RIGHT",
+    4 : "LEFT",
+    5 : "DOWN",
+    6 : "UPRIGHT",
+    7 : "UPLEFT",
+    8 : "DOWNRIGHT",
+    9 : "DOWNLEFT",
+    10 : "UPFIRE",
+    11 : "RIGHTFIRE",
+    12 : "LEFTFIRE",
+    13 : "DOWNFIRE",
+    14 : "UPRIGHTFIRE",
+    15 : "UPLEFTFIRE",
+    16 : "DOWNRIGHTFIRE",
+    17 : "DOWNLEFTFIRE",
+}
+        '''
+
+        self.action_space = action_space
+
+    def any_to_kill(self,observation):
+        ob = observation
+        #detect_wall()
+
+
+        
+    # You should modify this function
+    def act(self, observation, reward, done):
+
+        # obervation is 210 , 160 , 3 matrix which contains the RGB values
+        #print(self.action_space.sample())
+        # Does the agent know where to move 
+        # Does the agent know when and where to shoot 
+        # kill everyone and figure out the exit route
+
+
+        if self.done_ ==  24 :
+            np.savetxt('ob.txt', observation, fmt="%s")
+            f = open('sums.txt','a')
+            for rows in observation:
+                for col in rows:
+                    f.write(str(str(self.summation_it(col))))
+                f.write('\n')
+            f.close()
+        self.done_ += 1
+
+
+        return randint(10, 17)
+
+    def summation_it(self, on):
+        print(on)
+        sum = 0
+        for li in on:
+            sum += li
+        if sum == 0:
+            return 11
+        elif sum == 390:
+            return 22
+        elif sum == 484:
+            return 33
+        elif sum == 513:
+            return 44
+        else:
+            return 53
+        return sum
