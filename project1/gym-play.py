bb2f3a406ed772dbcb1d8827232f3e4d2d81195c
ketankokane94 <ketankokane94@gmail.com>
2019-03-02 21:11:52 -0500

    kept the main function same as provided by professor

--- project1/gym-play.py
+++ project1/gym-play.py
@@ -1,17 +1,19 @@
 import argparse
 import gym
 from gym import wrappers, logger
+import pandas as pd
 
 from random_agent import random_agent as Agent1
 from only_shooting_agent import only_shooting_agent as Agent2
-from agent import Agent
+from env_analysis_agent import Perceiving_Agent as Agent
 
 
 ## YOU MAY NOT MODIFY ANYTHING BELOW THIS LINE OR USE
 ## ANOTHER MAIN PROGRAM
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=None)
-    parser.add_argument('--env_id', nargs='?', default='Berzerk-v0', help='Select the environment to run')
+    parser.add_argument('--env_id', nargs='?', default='Berzerk-v0',
+                        help='Select the environment to run')
     args = parser.parse_args()
 
     # You can set the level to logger.DEBUG or logger.WARN if you
@@ -26,8 +28,7 @@ if __name__ == '__main__':
     # like: tempfile.mkdtemp().
     outdir = 'random-agent-results'
 
-
-    #env.seed(0)
+    env.seed(0)
     agent = Agent(env.action_space)
 
     episode_count = 100
@@ -38,12 +39,11 @@ if __name__ == '__main__':
     special_data['ale.lives'] = 3
     ob = env.reset()
     while not done:
-        
         action = agent.act(ob, reward, done)
         ob, reward, done, x = env.step(action)
         score += reward
         env.render()
-     
+
     # Close the env and write monitor result info to disk
-    print ("Your score: %d" % score)
+    print("Your score: %d" % score)
     env.close()
